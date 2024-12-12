788d3dd0fc868be0010e5f96c369c01df87ddc74
Damien Cren <68796664+damiencren@users.noreply.github.com>
2024-12-12 02:32:01 -0500

    add auth

---
+++ src/auth.ts
@@ -0,0 +1,22 @@
+// in auth.ts
+import GoogleProvider from "next-auth/providers/google"
+
+import { AuthOptions, getServerSession } from "next-auth"
+
+const authOptions: AuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET!
+}
+
+/**
+ * Helper function to get the session on the server without having to import the authOptions object every single time
+ * @returns The session object or null
+ */
+const getSession = () => getServerSession(authOptions)
+
+export { authOptions, getSession }
